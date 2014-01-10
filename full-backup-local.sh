46bdc251e501f5c521c5dcb8fd4009e5bdddb0ab
Geraint White <geraintwhite@gmail.com>
2014-01-10 21:38:55 +0000

    Update full-backup-local.sh

--- full-backup-local.sh
+++ full-backup-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -Pavz pi@raspberrypi:/home/pi/backups/backupFiles/fullBackup/fullBackup.tar.gz /home/geraint/backups/fullBackup.tar.gz
+rsync -Pavz pi@raspberrypi:/home/pi/backups/fullBackup/fullBackup.tar.gz /home/geraint/backups/fullBackup.tar.gz
