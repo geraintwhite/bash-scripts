c613f8e7e4fce2274b95aed231caeb93f32d62d8
Geraint White <geraintwhite@gmail.com>
2014-01-25 22:31:35 +0000

    Update block-sites.sh

--- block-sites.sh
+++ block-sites.sh
@@ -3,12 +3,15 @@
 usage="Usage: $(basename $0) [-ar domain] [-h]
 
 Options:
-    -a		--add		add site to block list
-    -r		--remove	remove site from block list
-    -h		--help		show this help text"
+    -a    --add       add site to block list
+    -r    --remove    remove site from block list
+    -l    --list      list all blocked sites
+    -h    --help      show this help text"
 
 argerr="$(basename $0): missing or unknown argument
 Try $(basename $0) --help for usage information"
+
+tag="#BlockedSites#"
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
@@ -16,11 +19,17 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 case "$1" in
+
     -a|--add)
-        if [ -n $2 ]; then
+        if [ -z "$2" ]; then
             echo "$argerr"
             exit 1
         fi
+
+        if ! grep -q $tag /etc/hosts; then
+            echo $tag | sudo tee -a /etc/hosts > /dev/null
+        fi
+
         if ! grep -q $2 /etc/hosts; then
             echo -e "127.0.0.1\t$2" | sudo tee -a /etc/hosts > /dev/null
             echo "Added $2 to /etc/hosts"
@@ -28,11 +37,13 @@ case "$1" in
             echo "$2 already in /etc/hosts"
         fi
         ;;
+
     -r|--remove)
-        if [ -n $2 ]; then
+        if [ -z "$2" ]; then
             echo "$argerr"
             exit 1
         fi
+
         if grep -q $2 /etc/hosts; then
             TEMP=$(mktemp)
             grep -v $2 /etc/hosts > $TEMP
@@ -42,10 +53,19 @@ case "$1" in
             echo "$2 not in /etc/hosts"
         fi
         ;;
+
+    -l|--list)
+        for line in "$(sed "0,/$tag/d" /etc/hosts)"; do
+            echo $(echo $line | sed -e 's/^127.0.0.1[ \t]*//')
+        done
+        ;;
+
     -h|--help)
         echo "$usage"
         ;;
+
     *)
         echo "$argerr"
         ;;
+
 esac
